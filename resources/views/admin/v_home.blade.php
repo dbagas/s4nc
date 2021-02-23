81444b123525fbfd527a60ae4f2124a8a45cbf87
dbagas17 <dbagas17@users.noreply.github.com>
2021-02-23 19:32:19 +0700

    first

---
+++ resources/views/admin/v_home.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.v_template')
+@section('content')
+    <h1>ini halaman home admin</h1>
+@endsection
---
+++ resources/views/layout/v_nav.blade.php
@@ -0,0 +1,41 @@
+<header class="header_area">
+    <div class="main_menu" id="mainNav">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <div class="container box_1620">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <a class="navbar-brand logo_h" href="index.html"><img src="" alt="">S4NC</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav menu_nav ml-auto">
+                        <li class="nav-item active"><a class="nav-link" href="/home">Home</a></li> 
+                        <li class="nav-item"><a class="nav-link" href="/about">About</a></li> 
+                        <li class="nav-item"><a class="nav-link" href="https://discord.com/invite/K7wq9Ah">Discord</a>
+                        <li class="nav-item submenu dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">TEAM</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="#">Meh96</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">BlackPanda</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">unex</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">spilner</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Tommeister23</a></li>
+                            </ul>
+                        </li> 
+                        <li class="nav-item submenu dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Division</a>
+                            <ul class="dropdown-menu">
+                                <li class="nav-item"><a class="nav-link" href="#">PUBGM</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">DOTA2</a></li>
+                            </ul>
+                        </li> 
+                        <li class="nav-item"><a class="nav-link" href="/contact">Contact</a></li>
+                    </ul>
+                </div> 
+            </div>
+        </nav>
+    </div>
+</header>
